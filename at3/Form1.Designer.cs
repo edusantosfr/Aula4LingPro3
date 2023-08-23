f05d84c1ed37bb7cdf68097dbec50a91b2ea50d5
edusantosfr <70604190+edusantosfr@users.noreply.github.com>
2023-08-23 17:13:25 -0300

    Add files via upload

---
+++ at3/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace at3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonResposta = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Maças que quer levar: ";
+            // 
+            // buttonResposta
+            // 
+            this.buttonResposta.Location = new System.Drawing.Point(221, 117);
+            this.buttonResposta.Name = "buttonResposta";
+            this.buttonResposta.Size = new System.Drawing.Size(93, 23);
+            this.buttonResposta.TabIndex = 2;
+            this.buttonResposta.Text = "Valor total";
+            this.buttonResposta.UseVisualStyleBackColor = true;
+            this.buttonResposta.Click += new System.EventHandler(this.buttonResposta_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "- de 10 Maças",
+            "+ de 10 Maças",
+            "+ de 20 Maças",
+            "+ de 30 Maças"});
+            this.comboBox1.Location = new System.Drawing.Point(207, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(125, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "Opção";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(207, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Digite aqui a quantidade exata:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonResposta);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonResposta;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ at3/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace at3
+{
+    public partial class Form1 : Form
+    {
+
+        double maca, valor;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonResposta_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                maca = double.Parse(textBox1.Text);
+                if (maca > 0 && maca <10)
+                {
+                    valor = maca * 2.2;
+                    MessageBox.Show("O valor é de R$" + valor + ".", "Valor Total");
+                }
+
+                if (maca <= 0)
+                {
+                    MessageBox.Show("Nenhum valor foi digitado ou é zero.", "Erro");
+                }
+
+                if (maca >= 10)
+                {
+                    MessageBox.Show("Troque de Opção antes de colocar a quantidade.", "Erro");
+                }
+
+            }
+
+            if (comboBox1.SelectedIndex == 1 )
+            {
+                maca = double.Parse(textBox1.Text);
+
+                if (maca >= 20 || maca <10)
+                {
+                    MessageBox.Show("Troque de Opção antes de colocar a quantidade.", "Erro");
+                }
+
+                else
+                {
+                    valor = maca * 2;
+                    MessageBox.Show("O valor é de R$" + valor + ".", "Valor Total");
+                }
+            }
+
+            if (comboBox1.SelectedIndex == 2)
+            {
+
+                maca = double.Parse(textBox1.Text);
+
+                if (maca >= 30 || maca < 20)
+                {
+                    MessageBox.Show("Troque de Opção antes de colocar a quantidade.", "Erro");
+                }
+
+                else
+                {
+                    valor = maca * 1.8;
+                    MessageBox.Show("O valor é de R$" + valor + ".", "Valor Total");
+                }
+            }
+
+            if (comboBox1.SelectedIndex == 3)
+            {
+                maca = double.Parse(textBox1.Text);
+
+                if (maca < 30)
+                {
+                    MessageBox.Show("Troque de Opção antes de colocar a quantidade.", "Erro");
+                }
+
+                if (maca >= 30)
+                {
+                    valor = maca * 1.5;
+                    MessageBox.Show("O valor é de R$" + valor + ".", "Valor Total");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxMaca_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
